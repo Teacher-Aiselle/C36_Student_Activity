57c1516ed43e10e39fc57b30ad08360e1f0d4795
Teacher-Aiselle <74968262+Teacher-Aiselle@users.noreply.github.com>
2021-10-20 22:06:23 +0800

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,38 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+  
+
+  //writes the button.mousePressed() function
+
+
+
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+// Display the greetings element and use it to greet
+//the player when the player has logged in.
+  
+}
